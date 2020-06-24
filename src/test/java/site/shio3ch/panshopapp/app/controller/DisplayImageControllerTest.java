966647be07f6728b729ec4866e942e6957c2cdf4
shio3ch <rrrrse13@gmail.com>
2020-06-24 21:32:17 +0900

    update: Add display controller test logic.

--- src/test/java/site/shio3ch/panshopapp/app/controller/DisplayImageControllerTest.java
+++ src/test/java/site/shio3ch/panshopapp/app/controller/DisplayImageControllerTest.java
@@ -11,14 +11,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class DisplayControllerTest {
+public class DisplayImageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void displayへリクエストを投げたら200OKが返却される() throws Exception {
-        this.mockMvc.perform(get("/display")).andExpect(status().isOk());
+    void displayImageへリクエストを投げたら200OKが返却される() throws Exception {
+        this.mockMvc.perform(get("/display-image").param("id","1")).andExpect(status().isOk());
     }
-
 }
